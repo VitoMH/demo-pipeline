c4fbb6ff357c28c633f17ed6a167732332e3747e
VitoMH <victor.meza.herrera@gmail.com>
2025-04-10 19:53:55 -0400

    add views ping

--- myproyect/urls.py
+++ myproyect/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myapp.views import ping
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('ping/', ping, name='ping'),
 ]
