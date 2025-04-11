5a672e035e0019384e03986947b9994fb5819500
VitoMH <victor.meza.herrera@gmail.com>
2025-04-10 20:03:07 -0400

    add Tests

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+#from myapp.views import sumar
 
-# Create your tests here.
+# Test del endpoint ping
+class PingTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_endpoint_ping_responde_ping_pong(self):
+        response = self.client.get('/ping/')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'ping': 'pong'})
+
+'''
+    Prueba para chequear los status_code en html
+    def test_home(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 404)
+'''
